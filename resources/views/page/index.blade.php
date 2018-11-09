0a770f3222144f08495ab7f1a88ad93934b1a30b
SergeyArtyukh <Sergey.Artyukh.1@gmail.com>
2018-11-09 22:38:57 +0200

    init project

---
+++ resources/views/page/index.blade.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="initial-scale=1,minimum-scale=1,maximum-scale=1,width=device-width">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="/fonts/fonts/fonts.css" />
+    <link rel="stylesheet" type="text/css" href="/fonts/fonts/fonts.min.css" />
+    <link rel="stylesheet" href="{{ config('app.source_uri').mix('style/ui.css') }}">
+    <link rel="stylesheet" href="{{ config('app.source_uri').mix('style/media_queries.css') }}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow|Roboto+Condensed" rel="stylesheet">
+
+    <title>Everad-group</title>
+
+</head>
+<body>
+    <div id="root">
+        <header class="uk-container">
+            <div class="tm-bg-header">
+                <div class="tm-logo-bar-container">
+                    <div class="tm-logo-box">
+                        <img src="/img/header/logo.png" alt="#">
+                        <h1>panto</h1>
+                    </div>
+                    <div class="tm-item2-logo-bar">
+                        <img src="/img/header/leafe-header.png" alt="#">
+                        <div>натурально</div>
+                    </div>
+                    <div class="tm-item3-logo-bar">
+                        <img src="/img/header/like-header.png" alt="#">
+                        <div>эффективно</div>
+                    </div>
+                </div>
+                <div class="tm-title-header">
+                    <p>Сколько раз за ночь ты можешь?  </p>
+                </div>
+                <div class="tm-flex-box-header">
+                    <div class="tm-flex-container1">
+                        <div class="tm-info-text">
+                            Пантомакс – каменный стояк<br>  для неприличных рекордов
+                        </div>
+                        <div class="tm-list-flex-header">
+                            <ul>
+                                <li>
+                                    <div><img src="/img/header/check-mark-header.png" alt="#"></div>
+                                    <p>Мощная эрекция и потенция</p>
+                                </li>
+                                <li>
+                                    <div><img src="/img/header/check-mark-header.png" alt="#"></div>
+                                    <p>Занимайся сексом сколько хочешь</p>
+                                </li>
+                                <li>
+                                    <div><img src="/img/header/check-mark-header.png" alt="#"></div>
+                                    <p>Получай космические оргазмы</p>
+                                </li>
+                                <li>
+                                    <div><img src="/img/header/check-mark-header.png" alt="#"></div>
+                                    <p>Гарантированный стояк<br>до 3-х часов</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="tm-product-container">
+                            <img src="/img/header/product-img4.png" alt="#">
+                        </div>
+                    </div>
+                    <div class="tm-flex-container2">
+                        <div>50%</div>
+                        <p>Ваша потенция вернется через 21 день<br> применения! Проверьте сами</p>
+                    </div>
+                    <div class="tm-flex-container3">
+                        <div class="tm-calculate-items-container">
+                            <div class="tm-calculate-items">
+                                <span>1</span>
+                                <span>0</span>
+                            </div>
+                            <div class="tm-text-calculate-box">
+                                <div class="tm-calculate-mark">*</div>
+                                <div class="tm-text-calculate">
+                                    Всего упаковок<br> по акции
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tm-form">
+                            <div class="tm-price-box uk-grid">
+                                <div class="tm-old-price uk-width-1-2">
+                                    <span class="tm-text-old-price">Полная цена</span><br>
+                                    <div>
+                                        <div class="tm-number-old-price">1980</div>
+                                        <span class="tm-rub-old-price">руб</span>
+                                    </div>
+                                </div>
+                                <div class="tm-new-price uk-width-1-2">
+                                    <span class="tm-text-new-price">Цена со скидкой</span><br>
+                                    <span class="tm-number-new-price">990</span>
+                                </div>
+                            </div>
+                            <form class="" action="index.html" method="post">
+                                <input type="text" name="" value="" placeholder="Введите ваше имя">
+                                <input type="number" name="" value="" placeholder="Введите номер телефона">
+                                <button type="button" name="button">Заказать сейчас</button>
+                            </form>
+                            <div class="tm-privacy-terms">
+                                <div><img src="/img/header/lock-form.png" alt="#">
+                                <div>Ваши данные под защитой SecureTECH</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <main class="uk-container">
+            <section class="tm-block2">
+                <div class="tm-text-block2">
+                    <div class="tm-title-text-block2">
+                        <h1>Проблема <span>ума</span> или <span>тела</span>?</h1>
+                    </div>
+                    <div class="tm-main-text-block2">
+                        <p>Ответьте всего на 2 вопроса и узнайте свой тип импотенции</p>
+                    </div>
+                </div>
+                <div class="tm-form-block2">
+                    <div class="tm-white-box-container">
+                        <div class="tm-white-box">
+                            <div class="tm-text-white-box"><span>1</span><p>как уменьшалась эрекция?</p></div>
+                            <div class="tm-red-form-box">
+                                <form class="tm-questions-form" action="index.html" method="get">
+                                    <button type="button" name="button">постепенно</button>
+                                    <button type="button" name="button">внезапно</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="tm-block3">
+                <div class="tm-text-block3-container">
+                    <h1>Panto<span>MAX</span> – максимальное<br>  удовольствие в сексе </h1>
+                    <p><span>Пантовые орешки</span> – экопродукт, в состав которого входят натуральные компоненты. Препарат абсолютно совместим с клеточными жидкостями, быстро проникает в кровь и начинает действовать уже через 30 минут. Биогенный комплекс орешек действует на половые и репродуктивные функции мужского организма, эндокринные функции, гормональный статус. Также Название оказывает многоплановое действие на функции
+                    и состояние мозга, сердечно-сосудистой, пищеварительной, нервной, иммунной систем.</p>
+                </div>
+            </section>
+            <section class="tm-block4">
+                <div class="tm-title-block4">
+                    <h1>пантовых орешек<span>panto</span></h1>
+                </div>
+                <div class="tm-list-items-container-block4">
+                    <div class="uk-text-center tm-mobile-box" uk-grid>
+                        <div class="uk-width-1-3 tm-list-item">
+                            <div class="tm-list-items-block4 uk-card uk-card-default uk-card-body">
+                                <div class="tm-list-img">
+                                    <img src="img/main/list-block4/list-item1.png" alt="">
+                                </div>
+                                <div class="tm-list-item-text uk-card-default uk-card-body">Дарить яркие <br>оргазмы</div>
+                            </div>
+                        </div>
+                        <div class="uk-width-1-3 tm-list-item">
+                            <div class="tm-list-items-block4 uk-card uk-card-default uk-card-body">
+                                <div class="tm-list-img">
+                                    <img src="img/main/list-block4/list-item2.png" alt="">
+                                </div>
+                                <div class="tm-list-item-text uk-card-default uk-card-body">Нормализует<br> гормональный фон</div>
+                            </div>
+                        </div>
+                        <div class="uk-width-1-3 tm-list-item">
+                            <div class="tm-list-items-block4 uk-card uk-card-default uk-card-body">
+                                <div class="tm-list-img">
+                                    <img src="img/main/list-block4/list-item3.png" alt="">
+                                </div>
+                                <div class="tm-list-item-text uk-card-default uk-card-body">Избавляет от эректильной<br>  дисфункции </div>
+                            </div>
+                        </div>
+                        <div class="uk-width-1-3 tm-list-item">
+                            <div class="tm-list-items-block4 uk-card uk-card-default uk-card-body">
+                                <div class="tm-list-img">
+                                    <img src="img/main/list-block4/list-item4.png" alt="">
+                                </div>
+                                <div class="tm-list-item-text1 uk-card-default uk-card-body" >Улучшает<br>  кровообращение<br>  в организме</div>
+                            </div>
+                        </div>
+                        <div class="uk-width-1-3 tm-list-item">
+                            <div class="tm-list-items-block4 uk-card uk-card-default uk-card-body">
+                                <div class="tm-list-img">
+                                    <img src="img/main/list-block4/list-item5.png" alt="">
+                                </div>
+                                <div class="tm-list-item-text uk-card-default uk-card-body">Повышает тонус  и жизненные силы</div>
+                            </div>
+                        </div>
+                        <div class="uk-width-1-3 tm-list-item">
+                            <div class="tm-list-items-block4 uk-card uk-card-default uk-card-body">
+                                <div class="tm-list-img">
+                                    <img src="img/main/list-block4/list-item6.png" alt="">
+                                </div>
+                                <div class="tm-list-item-text uk-card-default uk-card-body">Регулирует либидо<br>  и желание</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer class="uk-container">
+            <section class="tm-block5">
+                <div class="tm-text-footer-container">
+                    <h1>мнение<span>врача</span></h1>
+                    <p>50% мужчин до 35 лет имеют проблемы с эрекцией. Поэтому при первых же признаках необходимо брать быка за рога и решать проблему. </p>
+                    <p>Я своим пациентам советую пантовые орешки Пантомакс, это натуральный препарат, который отлично влияет не только на потенцию, но и на весь организм. </p>
+                    <p>При приеме Пантомакс у мужчины нормализуется работа сосудов и повышается потенция. Одним из действий препарата также является улучшение психоэмоциональной составляющей сексуальной жизни. Препарат поможет поддержать эрекцию во время всего полового акта. При этом эффект пантовых орешков не зависит от времени их приема.</p>
+                    <div>
+                        <span>Ильнур Мухазин, врач-андролог, к.м.н.</span>
+                        <img src="/img/footer/signature-doctor.png" alt="#">
+                    </div>
+                </div>
+            </section>
+        </footer>
+    </div>
+
+
+<script type="text/javascript" src="{{ config('app.source_uri').mix('js/home.js') }}"></script>
+<script src="uikit/dist/js/uikit-icons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.20/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.20/js/uikit-icons.min.js"></script>
+</body>
+</html>
